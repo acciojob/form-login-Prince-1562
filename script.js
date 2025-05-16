494213613c555f3ebf1dbd467bc3ec2bb53ae851
Prince-1562 <princemakani1562@gmail.com>
2025-05-16 11:12:19 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,14 @@
-function getFormvalue() {
-    //Write your code here
+function getFormvalue(event) {
+  event.preventDefault(); // Stop the form from submitting
 
+  const fname = document.forms[0].fname.value.trim();
+  const lname = document.forms[0].lname.value.trim();
+
+  const fullName = fname + " " + lname;
+
+  if (fname || lname) {
+    alert(fullName);
+  } else {
+    alert("Please enter your name.");
+  }
 }
